9bccfea4ccd6d58ae11d0a01b055e36b2b3c500d
CJ Carlson <cjcarlson@berkeley.edu>
2018-09-25 14:03:46 -0400

    name fix

--- R/hello.R
+++ R/hello.R
@@ -16,7 +16,7 @@
 
 curve.df <- function(assoc.df, iter){
 
-  u.hosts <- unique(assoc.df$Host)
+  u.hosts <- unique(assoc.df[,1])
   n.hosts <- length(u.hosts)
 
   results.df <- data.frame(n.host=0,n.par=0)
@@ -27,8 +27,8 @@ curve.df <- function(assoc.df, iter){
     for (j in 1:iter) {
 
       sub.hosts <- sample(u.hosts,i)
-      sub.df <- assoc.df[assoc.df$Host %in% sub.hosts,]
-      u.par <- unique(sub.df$Parasite)
+      sub.df <- assoc.df[assoc.df[,1] %in% sub.hosts,]
+      u.par <- unique(sub.df[,2])
       n.par <- length(u.par)
 
       results.df[nrow(results.df)+1,] <- c(i, n.par)
