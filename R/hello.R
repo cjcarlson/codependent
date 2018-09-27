f4ba29d6b38dddef8529fb7b9bcfc4c8f6819f34
CJ Carlson <cjcarlson@berkeley.edu>
2018-09-27 15:47:57 -0400

    some pretty serious changes
    mid-way through dealing with 50% sampling

--- R/hello.R
+++ R/hello.R
@@ -49,6 +49,7 @@ binera <- function(assoc.df, iter) {
 }
 
 
+
 copredict <- function(n.indep, assoc.df, iter1, iter2) {
 
   estlist <- c()
@@ -67,4 +68,50 @@ copredict <- function(n.indep, assoc.df, iter1, iter2) {
   print(paste("The upper 95% CI is",uci))
 
 
+}
+
+binera.50 <-  function(assoc.df, iter) {
+  
+  c <- curve.df(assoc.df, iter)
+  c <- c[c$n.host < max(c$n.host)*.5,]
+  model1 <- nls(n.par~b*n.host^z,start = list(b = 1, z = 0.5),data=c)
+  return(model1)
+  
+}
+
+
+copredict.ci <- function(n.indep, assoc.df, iter1, iter2) {
+  
+  estlist <- c()
+  est100list <- c()
+  df <- data.frame(blist = c(0), zlist=c(0))
+  for (i in 1:iter1) {
+    q <- coef(binera.50(assoc.df, iter2))
+    est <- q["b"] * (n.indep)^(q["z"])
+    est.100 <- q["b"] * (length(unique(assoc.df[,1])))^q["z"]
+    df[i,] <- c(q["b"],q["z"])
+    estlist <- c(estlist,est)
+    est100list <- c(est100list,est.100)
+  }
+  
+
+  est <- mean(estlist)
+  elnorm(estlist, method = "mvue", ci = TRUE, ci.type = "two-sided", 
+         ci.method = "exact", conf.level = 0.95)
+  lci <- est - 1.96*error
+  uci <- est + 1.96*error
+  
+  
+  est.100 <- mean(est100list)
+  elnorm(x, method = "mvue", ci = FALSE, ci.type = "two-sided", 
+         ci.method = "exact", conf.level = 0.95)
+                
+  print(paste(expression("Estimated number of species is"),est))
+  print(paste("The lower 95% CI is",lci))
+  print(paste("The upper 95% CI is",uci))
+  print(paste(expression("True number of species in entire network is"),length(unique(assoc.df[,1]))))
+  print(paste(expression("Estimated number of species in entire network is"),est.100))
+  print(paste("The lower 95% CI is",lci))
+  print(paste("The upper 95% CI is",uci))
+  
 }
