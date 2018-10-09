a10606bad4997e67a553b400ac1a8971873cc45b
CJ Carlson <cjcarlson@berkeley.edu>
2018-10-09 15:07:38 -0400

    package exports now

--- R/copredict.R
+++ R/copredict.R
@@ -7,10 +7,12 @@
 #' @param assoc.df A data frame of host-affiliate intractions, host names in the first column and affiliate names in the second
 #' @param iter1 Set the number of times to fit a curve
 #' @param iter2 Set the number of points to subsample at each host richness within the generation of each curve
+#'
+#' @export
 
 
 copredict <- function(n.indep, assoc.df, iter1, iter2) {
-  
+
   estlist <- c()
   for (i in 1:iter1) {
     q <- coef(binera(assoc.df, iter2))
@@ -18,7 +20,7 @@ copredict <- function(n.indep, assoc.df, iter1, iter2) {
     estlist <- c(estlist,est)
     print(i)
   }
-  
+
   est <- mean(estlist)
   hist(estlist,main='Extrapolated estimates')
   error <- qt(0.975,df=iter1-1)*sd(estlist)/sqrt(iter1)
@@ -27,6 +29,6 @@ copredict <- function(n.indep, assoc.df, iter1, iter2) {
   print(paste(expression("Estimated number of species is"),est))
   print(paste("The lower 95% CI is",lci))
   print(paste("The upper 95% CI is",uci))
-  
-  
+
+
 }
--- R/curvedf.R
+++ R/curvedf.R
@@ -5,25 +5,26 @@
 #'
 #' @param assoc.df The raw dataset of associations (pairs of names in two columns, hosts and affiliates)
 #' @param iter Number of iterations required for each subsample
-#' 
+#'
+#' @export
 
 curve.df <- function(assoc.df, iter){
-  
+
   u.hosts <- unique(assoc.df[,1])
   n.hosts <- length(u.hosts)
-  
+
   results.df <- data.frame(n.host=0,n.par=0)
   results.df <- results.df[-1,]
-  
+
   for (i in 1:n.hosts) {
     #for (i in 1:5) {
     for (j in 1:iter) {
-      
+
       sub.hosts <- sample(u.hosts,i)
       sub.df <- assoc.df[assoc.df[,1] %in% sub.hosts,]
       u.par <- unique(sub.df[,2])
       n.par <- length(u.par)
-      
+
       results.df[nrow(results.df)+1,] <- c(i, n.par)
     }
     #print(i)
