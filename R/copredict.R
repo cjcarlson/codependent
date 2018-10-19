d3dbe4c2760dcb4cb21b76546f4cfa7286856f3f
taddallas <tdallas@ucdavis.edu>
2018-10-19 17:26:28 +0300

    some changes to package structure

--- R/copredict.R
+++ R/copredict.R
@@ -7,28 +7,26 @@
 #' @param assoc.df A data frame of host-affiliate intractions, host names in the first column and affiliate names in the second
 #' @param iter1 Set the number of times to fit a curve
 #' @param iter2 Set the number of points to subsample at each host richness within the generation of each curve
+#' @param plot (boolean; default is TRUE) plot results?
 #'
 #' @export
 
 
-copredict <- function(n.indep, assoc.df, iter1, iter2) {
-
-  estlist <- c()
-  for (i in 1:iter1) {
-    q <- coef(binera(assoc.df, iter2))
-    est <- q["b"] * (n.indep)^(q["z"])
-    estlist <- c(estlist,est)
-    print(i)
-  }
-
+copredict <- function(n.indep, assoc.df, 
+	iter1, iter2, plot=TRUE) {
+	estlist <- sapply(1:iter1, function(x){
+		q <- coef(binera(assoc.df, iter2))
+    q["b"] * (n.indep)^(q["z"])
+	})
   est <- mean(estlist)
-  hist(estlist,main='Extrapolated estimates')
-  error <- qt(0.975,df=iter1-1)*sd(estlist)/sqrt(iter1)
+	if(plot){
+	  hist(estlist, main='Extrapolated estimates')
+	}
+  error <- qt(0.975, df=iter1-1) * sd(estlist)/sqrt(iter1)
   lci <- est - 1.96*error
   uci <- est + 1.96*error
-  print(paste(expression("Estimated number of species is"),est))
-  print(paste("The lower 95% CI is",lci))
-  print(paste("The upper 95% CI is",uci))
-
-
+#  print(paste(expression("Estimated number of species is"),est))
+#  print(paste("The lower 95% CI is",lci))
+#  print(paste("The upper 95% CI is",uci))
+	return(c(mean=est, lowerCI=lci, upperCI=uci))
 }
--- R/copredict50.R
+++ R/copredict50.R
@@ -8,11 +8,13 @@
 #' @param assoc.df A data frame of host-affiliate intractions, host names in the first column and affiliate names in the second
 #' @param iter1 Set the number of times to fit a curve
 #' @param iter2 Set the number of points to subsample at each host richness within the generation of each curve
+#' @param plot (boolean; default is TRUE) plot results?
 #'
 #' @export
 
 
-copredict.ci <- function(n.indep, assoc.df, iter1, iter2) {
+copredict.ci <- function(n.indep, assoc.df, 
+	iter1, iter2, plot=TRUE) {
 
   estlist <- c()
   est100list <- c()
@@ -27,35 +29,39 @@ copredict.ci <- function(n.indep, assoc.df, iter1, iter2) {
     print(i)
   }
 
-
   e2 <- elnorm(est100list, method = "mvue", ci = TRUE, ci.type = "two-sided",
                ci.method = "exact", conf.level = 0.95)
   est2 <- exp(e2$parameters[1])
   lci2 <- exp(e2$interval$limits[1])
   uci2 <- exp(e2$interval$limits[2])
 
-  print(paste(expression("True number of species in entire network is"),length(unique(assoc.df[,2]))))
-  print(paste(expression("Estimated number of species in entire network is"),est2))
-  print(paste("The lower 95% CI is",lci2))
-  print(paste("The upper 95% CI is",uci2))
+  print(paste(expression("True number of species in entire network is "),length(unique(assoc.df[,2]))))
+  print(paste(expression("Estimated number of species in entire network is "),est2))
+  print(paste("The lower 95% CI is ",lci2))
+  print(paste("The upper 95% CI is ",uci2))
 
 
-  par(mfrow=c(2,1))
-  hist(est100list,main='100% estimates')
-  hist(estlist,main='Extrapolated estimates')
+	if(plot){
+		par(mfrow=c(2,1))
+		hist(est100list,main='100% estimates')
+		hist(estlist,main='Extrapolated estimates')
+	}
 
   e1 <- elnorm(estlist, method = "mvue", ci = TRUE, ci.type = "two-sided",
                ci.method = "exact", conf.level = 0.95)
+
   est <- exp(e1$parameters[1])
   lci <- exp(e1$interval$limits[1])
   uci <- exp(e1$interval$limits[2])
 
+  print(paste(expression("Extrapolated estimated number of species is "),est))
+  print(paste("The lower 95% CI is ",lci))
+  print(paste("The upper 95% CI is ",uci))
 
-
-  print(paste(expression("Extrapolated estimated number of species is"),est))
-  print(paste("The lower 95% CI is",lci))
-  print(paste("The upper 95% CI is",uci))
-
-
-
+	ret <- data.frame(
+		richness=c('True species richness', 'Estimated richness', 'Extrapolated richness'),
+		estimate = c(length(unique(assoc.df[,2])), est2, est),
+		lowerCI = c(NA, lci2, lci), 
+		upperCI = c(NA, uci2, uci)) 
+	return(ret)
 }
